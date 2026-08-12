f99e28de1d16d1056a639308f255c38c8db8d1f9
Snigdha K P <4MH23CS158@gmail.com>
2026-08-12 11:01:51 +0530

    fgerngg

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+
+        # Production frontend
+    "https://campus-ai-frontend-six.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
